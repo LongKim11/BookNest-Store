20e1e7863e9f6fc2f0a1a5e4754e8d774e61e86f
Long Kim <vokimlong11.cs@gmail.com>
2024-09-05 15:51:40 +0700

    Connect to DB and create model using mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
-const PORT = 8080;
+const PORT = 5555;
 
-export {PORT};
+const mongoDBURL = "mongodb+srv://root:root@bookstoredb.kvf7g.mongodb.net/?retryWrites=true&w=majority&appName=BookstoreDB"
+
+export {PORT, mongoDBURL};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
-import express from "express"
-import {PORT} from "./config.js"
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./model/bookModel.js";
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello world")
-})
+  res.status(200).send("Hello world");
+});
 
 app.listen(PORT, () => {
-    console.log(`App is listening at http://localhost:${PORT}`)
-})
+  console.log(`App is listening at http://localhost:${PORT}`);
+});
+
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(() => {
+//     console.log("App connected to database");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
