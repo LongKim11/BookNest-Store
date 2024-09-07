8629e609b75eff2b9f67d49ecb2ad8d9df759942
Long Vo <vokimlong11.cs@gmail.com>
2024-09-07 10:27:39 +0700

    Add middleware for handling CORS policy

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 // Convert JSON string in payload to JSON object and parse to req.body
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
@@ -14,14 +17,13 @@ app.get("/", (req, res) => {
 
 app.use("/books", booksRoute);
 
-app.listen(PORT, () => {
-  console.log(`App is listening at http://localhost:${PORT}`);
-});
-
 mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening at http://localhost:${PORT}`);
+    });
   })
   .catch((error) => {
     console.log(error);
