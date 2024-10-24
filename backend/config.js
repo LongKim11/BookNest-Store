b0958f907e76ea3aef5c9e5f37d95029b9985df1
Long Vo <vokimlong11.cs@gmail.com>
2024-10-24 21:59:02 +0700

    Add dockerfile and docker-compose

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,8 @@
 const PORT = 8092;
 
-const mongoDBURL =
-  "mongodb+srv://root:TDTU@bookstoredb.czkl4.mongodb.net/?retryWrites=true&w=majority&appName=BookstoreDB";
+// const mongoDBURL =
+//   "mongodb+srv://root:TDTU@bookstoredb.czkl4.mongodb.net/?retryWrites=true&w=majority&appName=BookstoreDB";
+
+const mongoDBURL = "mongodb://mongo:27017/BookstoreDB";
 
 export { PORT, mongoDBURL };
